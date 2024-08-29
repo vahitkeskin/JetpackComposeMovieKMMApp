eb621f68e17285a45b5f5c5c90c55a74654f51fc
Vahit Keskin <vhtkskn63@gmail.com>
2024-08-29 09:11:53 +0300

    - From api request/response kotlin ktor api class

---
+++ shared/src/commonMain/kotlin/com/vahitkeskin/jetpackcomposemoviekmmapp/data/remote/KtorApi.kt
@@ -0,0 +1,9 @@
+package com.vahitkeskin.jetpackcomposemoviekmmapp.data.remote
+
+//API_KEY -> https://www.themoviedb.org/settings/api
+//https://api.themoviedb.org/3/movie/603692?api_key=API_KEY
+//https://api.themoviedb.org/3/movie/popular?page=1&api_key=API_KEY
+
+internal annotation class KtorApi {
+
+}

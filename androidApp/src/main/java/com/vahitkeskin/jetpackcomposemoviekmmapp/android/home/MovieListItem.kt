7030761fef315fb8991a0374ae4ba60e82d6f931
Vahit Keskin <vhtkskn63@gmail.com>
2024-08-30 21:43:04 +0300

    - Add pull refresh and home screen revised.

---
+++ androidApp/src/main/java/com/vahitkeskin/jetpackcomposemoviekmmapp/android/home/MovieListItem.kt
@@ -0,0 +1,91 @@
+package com.vahitkeskin.jetpackcomposemoviekmmapp.android.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.vahitkeskin.jetpackcomposemoviekmmapp.android.R
+import com.vahitkeskin.jetpackcomposemoviekmmapp.domain.model.Movie
+
+@Composable
+fun MovieListItem(
+    modifier: Modifier = Modifier,
+    movie: Movie,
+    onMovieClick: (Movie) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .height(220.dp)
+            .clickable { onMovieClick(movie) },
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column {
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                AsyncImage(
+                    model = movie.imageUrl,
+                    contentDescription = movie.title,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(bottomStart = 2.dp, bottomEnd = 2.dp))
+                )
+
+                Surface(
+                    color = Color.Black.copy(alpha = 0.6f),
+                    modifier = Modifier.size(50.dp),
+                    shape = CircleShape
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.play_button),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(12.dp)
+                            .align(Alignment.Center)
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = movie.releaseDate, style = MaterialTheme.typography.caption)
+            }
+
+        }
+
+    }
+
+}
